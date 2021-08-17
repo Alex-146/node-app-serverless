aa64ec58aaac6913480708d13b38569d89b52d8b
alex-146 <deynekos1488@gmail.com>
2021-08-17 11:36:31 +0300

    works with arrays

--- src/providers/parsePrice.js
+++ src/providers/parsePrice.js
@@ -1,35 +1,50 @@
 const axios = require("axios");
 
+function proceedClass(classFrom, data) {
+  const { error, form } = data;
+
+  if (error) {
+    return {
+      title: classFrom,
+      error: data.message ?? "Request error"
+    }
+  }
+
+  const { systemFromTitle, amountFrom, currFrom,
+    systemToTitle, amountTo, currTo, 
+    actualRate, rateFrom } = form;
+
+  let inverse = amountFrom * rateFrom;
+  //inverse = amountFrom / actualRate;
+
+  return {
+    title: `${systemFromTitle} - ${systemToTitle}`,
+    a: `${amountFrom} ${currFrom} = ${amountTo} ${currTo}`,
+    b: `${inverse} ${currFrom} = ${amountFrom} ${currTo}`
+  }
+}
+
 async function parsePrice(classFrom) {
-    const body = {
-      classFrom,
+  if (!Array.isArray(classFrom)) {
+    classFrom = [classFrom];
+  }
+
+  const responses = await Promise.all(classFrom.map(classValue => { 
+    return axios.post("https://smartwm.biz/exchange/rate", {
+      "classFrom": classValue,
       "classTo": "privatuah_auto",
       "amountFrom": 100,
       "activeDirection": "from"
-    }
-    const { data } = await axios.post("https://smartwm.biz/exchange/rate", body);
+    });
+  }));
 
-    const { error, form } = data;
+  const result = responses.map(response => response.data)
+  .map((data, i) => proceedClass(classFrom[i], data));
 
-    if (error) {
-      return {
-        title: classFrom,
-        error: data.message ?? "Request error"
-      }
-    }
-
-    const { systemFromTitle, amountFrom, currFrom,
-      systemToTitle, amountTo, currTo, 
-      actualRate, rateFrom } = form;
-
-    let inverse = amountFrom * rateFrom;
-    //inverse = amountFrom / actualRate;
-
-    return {
-      title: `${systemFromTitle} - ${systemToTitle}`,
-      a: `${amountFrom} ${currFrom} = ${amountTo} ${currTo}`,
-      b: `${inverse} ${currFrom} = ${amountFrom} ${currTo}`
-    }
+  if (result.length === 1) {
+    return result[0];
+  }
+  return result;
 }
 
 module.exports = parsePrice;
